e59e339d995fd74e3534e49fdc7aa3027d354489
SoufianeBoukrichy <soufiane.boukrichy@gmail.com>
2026-03-04 22:15:56 +0000

    Initial DevOps lab: Flask + PostgreSQL Docker Compose stack

---
+++ README.md
@@ -0,0 +1,39 @@
+# DevOps Lab Project
+
+## Author
+Soufiane Boukrichy
+
+## Project Objective
+Build a DevOps-oriented infrastructure using Docker, Linux and monitoring tools.
+
+## Architecture
+
+Flask Application
+|
+Docker Compose
+|
+PostgreSQL Database
+
+## Technologies Used
+
+- Ubuntu Server
+- Docker
+- Docker Compose
+- Flask
+- PostgreSQL
+- Zabbix Monitoring
+- Git Version Control
+
+## Features
+
+- Multi-container architecture
+- Container networking
+- Persistent database volumes
+- Application containerization
+
+## Future Improvements
+
+- NGINX Reverse Proxy
+- CI/CD pipeline
+- Automation scripts
+- Cloud deployment (Azure)
---
+++ app/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASS = os.getenv("POSTGRES_PASSWORD")
+
+@app.route("/")
+def home():
+    try:
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASS
+        )
+        return "Flask connected to PostgreSQL successfully!"
+    except Exception as e:
+        return f"Database connection failed: {e}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
